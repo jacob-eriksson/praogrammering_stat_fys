ebbe199a79c6d4bddf322f30d60e7322928f5772
jacob-eriksson <130381262+jacob-eriksson@users.noreply.github.com>
2026-03-06 15:40:05 +0100

    Update fput.py

--- fput.py
+++ fput.py
@@ -1,5 +1,23 @@
-# FPUT Code here
+
 import numpy as np
 
-N = 32
+N = 31
 A = 2*np.eye(N) - np.eye(N, k=-1) - np.eye(N, k=1)
+
+
+print(A)
+
+eigenvalues, eigenvectors = np.linalg.eig(A)
+
+print("Eigenvalues:")
+print(eigenvalues)
+
+
+print("  ")
+print("  ")
+print("  ")
+print("  ")
+
+
+print("Eigenvectors:")
+print(eigenvectors)
