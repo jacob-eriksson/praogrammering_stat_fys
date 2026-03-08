9dd6cbfe51a61d618610971aef8829b97b60f19f
antarcticexplorer <ga5705co-s@student.lu.se>
2026-03-08 20:59:53 +0100

    Update fput.py

--- fput.py
+++ fput.py
@@ -1,15 +1,25 @@
-
 import numpy as np
 
+# Setting N
 N = 32
+
+# Initialisng A matrix, dimension (N-1)
 A = 2*np.eye(N - 1) - np.eye(N - 1, k=-1) - np.eye(N - 1, k=1)
 
+# Extracting eigenproperties
 eigvals, eigvecs = np.linalg.eig(A)
-eigvals_sorted = np.sort(eigvals)
 
+# Analytical eigenvalues for Task 2
 n_vals = np.array(list(range(1, N)))
 analytical_eigvals = (2 * np.sin(n_vals * np.pi / (2*N)))**2
 
-print(eigvals_sorted)
-print()
-print(analytical_eigvals)
+# Sorting eigenvalues
+eigvals_sorted = np.sort(eigvals)
+eigvecs_sorted = np.sort(eigvecs)
+
+# Task 2 comparison
+print(f"Analytical: {analytical_eigvals}")
+print(f"Numerical: {eigvals_sorted}")
+
+# Task 3)
+print(eigvecs_sorted)
